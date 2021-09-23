c004663cdc7ed1576afe2e4d4fc95fb575cdaa27
Richard Dowdy <richard.dowdy0@gmail.com>
2021-09-23 08:23:40 -0700

    working on chart

--- src/components/InvestmentChart.js
+++ src/components/InvestmentChart.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BarChart, CartesianGrid, XAxis, YAxis, Legend, Tooltip, Bar } from "recharts";
 import { calculateCompoundedInterestReturns } from "../utils/investmentHelpers";
 
 export default function InvestmentChart({ investmentData }) {
   console.log("From chart", investmentData);
+  const [chartData, setChartData] = useState(null);
   const { principal, duration, contribution, expectedInterest, frequency } = investmentData;
-  const data = calculateCompoundedInterestReturns(principal, duration, expectedInterest);
+  // const data = calculateCompoundedInterestReturns(principal, duration, expectedInterest);
+
+  useEffect(() => {
+    setChartData(calculateCompoundedInterestReturns(principal, duration, expectedInterest, contribution, frequency));
+  }, [investmentData]);
 
   return (
     <>
-      <BarChart width={1200} height={250} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="start" stackId="a" fill="#8884d8" />
-        <Bar dataKey="interestEarned" stackId="a" fill="#82ca9d" />
-      </BarChart>
+      {chartData && (
+        <>
+          <p>{`Your original investment of $${principal} has turned into $${
+            chartData[chartData.length - 1].yearEndTotal
+          }`}</p>
+          <BarChart width={650} height={300} data={chartData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="start" stackId="a" fill="#8884d8" />
+            <Bar dataKey="interestEarned" stackId="a" fill="#82ca9d" />
+          </BarChart>
+        </>
+      )}
     </>
   );
 }
