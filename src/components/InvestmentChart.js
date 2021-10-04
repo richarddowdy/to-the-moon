1dc266f99e43ee36dca6480acb74e164c70ad79d
Richard Dowdy <richard.dowdy0@gmail.com>
2021-10-03 21:29:57 -0700

    jenky fix for the legend overlap issue

--- src/components/InvestmentChart.js
+++ src/components/InvestmentChart.js
@@ -4,22 +4,28 @@ import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Legend, Too
 export default function InvestmentChart({ chartData }) {
   // console.log("From chart", chartData);
   const principal = chartData[0].principal;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log("bar render");
+    if (chartData) setData(chartData);
+  }, [chartData]);
 
   return (
     <>
-      {chartData && (
+      {data && (
         <>
-          <p>{`Your original investment of $${principal} has turned into $${
-            chartData[chartData.length - 1].yearEndTotal
-          }`}</p>
+          <h3 className="mb-4 mx-auto">{`Your original investment of $${principal} has turned into $${
+            data[data.length - 1].yearEndTotal
+          }`}</h3>
           <ResponsiveContainer width="100%" height={300}>
-            <BarChart className="ml-2" data={chartData}>
+            <BarChart className="ml-2" data={data}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="year" xAxisId={0} />
               <XAxis dataKey="year" xAxisId={1} hide />
               <YAxis />
               <Tooltip />
-              <Legend />
+              <Legend wrapperStyle={{ position: "relative" }} />
               <Bar dataKey="yearEndTotal" name="Total" xAxisId={1} fill="black" fillOpacity={0} />
               <Bar dataKey="principal" name="Principal" stackId="a" fill="#0088FE" />
               <Bar dataKey="totalContribution" name="Total Contribution" stackId="a" fill="#00C49F" />
